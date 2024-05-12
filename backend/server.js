3de015538197856618bed1592ddf1b5b863e66db
AVCastillo616 <arcastillo1@up.edu.ph>
2024-05-13 00:46:47 +0800

    fix:trying cors 16

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,19 @@ const app = express()
 //middlewares
 app.use(cors({
     origin: 'https://uplbattedancetracker.vercel.app/',
-    credentials:true
+    credentials:true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization',"Access-Control-Allow-Headers,","Access-Control-Allow-Methods","Origin","Accept"]
 }))
 
 // // allow CORS
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://uplbattedancetracker.vercel.app/");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Origin,Accept,Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Credentials","true");
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "https://uplbattedancetracker.vercel.app/");
+//     res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers,Access-Control-Allow-Methods,Origin,Accept,Content-Type, Authorization");
+//     res.setHeader("Access-Control-Allow-Credentials","true");
+//     next();
+//   });
 
 
 
