501ad8bae3a9ad49217d06cb89606e4f1a964af0
AVCastillo616 <arcastillo1@up.edu.ph>
2024-05-20 23:29:24 +0800

    fix: fixed signup issue

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -226,45 +226,65 @@ const Signup = ()=>{
         if(role==="Teacher"){ 
            if((firstName.length > 0 &&lastName.length > 0 &&role.length > 0 &&sex.length > 0 &&email.length > 0 &&password.length)>0){
                 newuser= {firstName,lastName,studentNum:"",role,sex,email,password}
+                try{
+                    await axios.post(`${process.env.REACT_APP_API_SERVER}/api/signup`,newuser,{withCredentials:true}).then(res =>{})
+                    setFname('')
+                    setLname('')
+                    setStudnum('')
+                    setRole('')
+                    setSex('')
+                    setEmail('')
+                    setPassword('')
+                    setDisableStudnum(false)
+                    setSnackbarLabel('New Student Added')
+                    setSuccess("success")
+                    setOpenSnack(true)
+    
+    
+                    
+                }catch(e){
+                    setSnackbarLabel(e.response.data.error)
+                    setSuccess("error")
+                    setOpenSnack(true)
+                }
            }else{
             setSnackbarLabel("Fill out all fields!")
             setSuccess("error")
             setOpenSnack(true)
            }
-        }else{
+        }else if(role==='Student'){
             if((firstName.length > 0 &&lastName.length > 0 &&studentNum.length > 0 &&role.length > 0 &&sex.length > 0 &&email.length > 0 &&password.length)>0){
                 newuser= {firstName,lastName,studentNum,role,sex,email,password}
+                try{
+                    await axios.post(`${process.env.REACT_APP_API_SERVER}/api/signup`,newuser,{withCredentials:true}).then(res =>{})
+                    setFname('')
+                    setLname('')
+                    setStudnum('')
+                    setRole('')
+                    setSex('')
+                    setEmail('')
+                    setPassword('')
+                    setDisableStudnum(false)
+                    setSnackbarLabel('New Student Added')
+                    setSuccess("success")
+                    setOpenSnack(true)
+    
+    
+                    
+                }catch(e){
+                    setSnackbarLabel(e.response.data.error)
+                    setSuccess("error")
+                    setOpenSnack(true)
+                }
             }else{
                 setSnackbarLabel("Fill out all fields!")
-            setSuccess("error")
-            setOpenSnack(true)
+                setSuccess("error")
+                setOpenSnack(true)
             }
         }   
 
     
-            try{
-                await axios.post(`${process.env.REACT_APP_API_SERVER}/api/signup`,newuser,{withCredentials:true}).then(res =>{
-        
-                })
-                setFname('')
-                setLname('')
-                setStudnum('')
-                setRole('')
-                setSex('')
-                setEmail('')
-                setPassword('')
-                setDisableStudnum(false)
-                setSnackbarLabel('New Student Added')
-                setSuccess("success")
-                setOpenSnack(true)
-
-
-                
-            }catch(e){
-                setSnackbarLabel(e.response.data.error)
-                setSuccess("error")
-                setOpenSnack(true)
-            }
+            
         
     }
 
